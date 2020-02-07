47b419caa24cf88d185d7d0380656149d0d1e984
dannyunico <danielbermudezreyes@gmail.com>
2020-02-06 20:56:00 -0400

    category

--- assests/js/index.js
+++ assests/js/index.js
@@ -30,3 +30,36 @@ $('.slide-servicio').slick({
     }
   ]
 });
+
+$('.detalle-slide').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  arrows: true,
+   autoplay: true,
+
+  responsive: [
+    {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
